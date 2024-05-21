1f43870aa6e11ff93293048ba795d9b1e2ecdb7e
Gino <gino.laranjeiras@onikel.com>
2024-05-21 16:06:28 -0300

    feat: Update UpdateUser handler to modify existing user document

--- handler/updateUser.go
+++ handler/updateUser.go
@@ -22,6 +22,7 @@ func (handler *DefaultHandler) UpdateUser(c *gin.Context) {
 	collectionName := "users"
 
 	var user models.User
+	var existingUser models.User
 
 	ctx := context.Background()
 
@@ -56,18 +57,37 @@ func (handler *DefaultHandler) UpdateUser(c *gin.Context) {
 
 	// Use the first document found (there should be only one)
 	doc := docs[0]
+	doc.DataTo(&existingUser)
 
-	user.UpdatedAt = time.Now()
-	user.Id = id
-	user.Uid = id
-	user.CreatedAt = doc.Data()["createdAt"].(time.Time)
+	if user.UserDisplayName != "" {
+		existingUser.UserDisplayName = user.UserDisplayName
+	}
+	if user.UserEmail != "" {
+		existingUser.UserEmail = user.UserEmail
+	}
+	if user.UserPhotoUrl != "" {
+		existingUser.UserPhotoUrl = user.UserPhotoUrl
+	}
+	if user.UserName != "" {
+		existingUser.UserName = user.UserName
+	}
+	if len(user.UserProperties) > 0 {
+		existingUser.UserProperties = user.UserProperties
+	}
+	if len(user.Addresses) > 0 {
+		existingUser.Addresses = user.Addresses
+	}
+	if (user.SelectedAddress != models.Address{}) {
+		existingUser.SelectedAddress = user.SelectedAddress
+	}
+	existingUser.UpdatedAt = time.Now()
 
-	_, err = dbClient.Collection(collectionName).Doc(doc.Ref.ID).Set(ctx, user)
+	_, err = dbClient.Collection(collectionName).Doc(doc.Ref.ID).Set(ctx, existingUser)
 
 	if err != nil {
 		sendError(c, http.StatusInternalServerError, err.Error())
 		return
 	}
 
-	sendSuccess(c, "user updated", http.StatusOK, user)
+	sendSuccess(c, "user updated", http.StatusOK, existingUser)
 }
