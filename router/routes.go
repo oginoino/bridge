5f829e48170fd1fccc49ad638b65c4b82e632b34
Gino <gino.laranjeiras@onikel.com>
2024-05-20 22:36:11 -0300

    feat: Add CORS middleware for handling cross-origin requests

--- router/routes.go
+++ router/routes.go
@@ -2,10 +2,34 @@ package router
 
 import (
 	"github.com/GinoCodeSpace/bridge/handler"
+	"github.com/gin-contrib/cors"
 
 	"github.com/gin-gonic/gin"
 )
 
 func initializeRoutes(router *gin.Engine) {
-	router.GET("/ping", handler.Ping)
+	// ctx := context.Background()
+
+	// db, authClient := handler.InitializeHandler()
+
+	corsConfig := cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Access-Control-Allow-Headers", "Origin", "X-Requested-With", "Content-Type", "Accept", "Authorization", "Access-Control-Allow-Origin"},
+		AllowCredentials: true,
+	}
+
+	router.Use(cors.New(corsConfig))
+
+	authorized := router.Group("api/v1/")
+
+	var authHandler *handler.AuthHandler
+
+	authorized.Use(authHandler.AuthMiddleware())
+
+	// router.GET("/ping", handler.Ping)
+
+	{
+		authorized.GET("/ping", handler.Ping)
+	}
 }
